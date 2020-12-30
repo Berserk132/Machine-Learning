e1e2f84f5072d939a0ad3bf1c0d516f8f7a0d79a
Berserk132 <ahmedsayed22988@yahoo.com>
2020-12-30 09:54:18 +0200

    Added Entropy

--- Assignment 2/Task 1/Assignment2-Task1.py
+++ Assignment 2/Task 1/Assignment2-Task1.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[37]:
 
 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 
 
-# In[3]:
+# In[38]:
 
 
 attributes =  ['vote_result', 'vote1', 'vote2', 'vote3', 'vote4', 'vote5', 'vote6', 'vote7', 'vote8', 'vote9', 'vote10', 
@@ -19,7 +20,7 @@ data.columns = attributes
 data.head()
 
 
-# In[23]:
+# In[39]:
 
 
 for index, row in data.iterrows():
@@ -45,13 +46,13 @@ for index, row in data.iterrows():
     data.loc[index] = rowData
 
 
-# In[25]:
+# In[40]:
 
 
 data.head()
 
 
-# In[28]:
+# In[41]:
 
 
 class Node():
@@ -63,16 +64,33 @@ class Node():
         self.predict = None
 
 
-# In[ ]:
+# In[51]:
 
 
+def calculate_entropy(df, predict_attr):
+    # Dataframe and number of republican/democrat examples in the data
+    r_df = df[df[predict_attr] == 'republican']
+    d_df = df[df[predict_attr] == 'democrat']
+    
+    # Get number of rows
+    r = float(r_df.shape[0])
+    d = float(d_df.shape[0])
+    
+    # Calculate entropy
+    if r  == 0 or d == 0:
+        entropy = 0
+    elif r == d:
+        entropy = 1
+    else:
+        entropy = ((-1*r)/(r + d))*math.log(r/(r+d), 2) + ((-1*d)/(r + d))*math.log(d/(r+d), 2)
+    return entropy
 
 
-
-# In[ ]:
-
+# In[54]:
 
 
+entropy = calculate_entropy(data, 'vote_result')
+print(entropy)
 
 
 # In[ ]:
